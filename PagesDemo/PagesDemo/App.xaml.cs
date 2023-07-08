386196a8483402998e6482df902b6d601f852e68
Ranjith <Ranjith.Rajendiran-ext@bshg.com>
2023-07-08 22:56:06 +0530

    Navigation concept discussed

---
+++ PagesDemo/PagesDemo/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace PagesDemo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		var navPage = new NavigationPage(new MainPage());
+		navPage.BarBackground = Colors.Chocolate;
+		navPage.BarTextColor = Colors.White;
+		MainPage = navPage;
+	}
+}
+
