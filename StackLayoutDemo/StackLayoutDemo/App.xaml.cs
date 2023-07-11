20ee1d2f7e1298f0ae269ba9b12c9532b01af449
Ranjith <Ranjith.Rajendiran-ext@bshg.com>
2023-07-11 11:34:12 +0530

    StackLayout Demo created

---
+++ StackLayoutDemo/StackLayoutDemo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace StackLayoutDemo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new StackLayoutDemo();
+	}
+}
+
---
+++ StackLayoutDemo/StackLayoutDemo/StackLayoutDemo.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace StackLayoutDemo;
+
+public partial class StackLayoutDemo : ContentPage
+{
+	public StackLayoutDemo()
+	{
+		InitializeComponent();
+	}
+}
