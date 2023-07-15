7a082df0d1dd7afd7207727597bd97971229e9a9
Ranjith <Ranjith.Rajendiran-ext@bshg.com>
2023-07-15 20:33:14 +0530

    TextControlsDemo and ActivityControls Demo added

---
+++ ControlsDemo/ControlsDemo/ActivityControlsDemo.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ControlsDemo;
+
+public partial class ActivityControlsDemo : ContentPage
+{
+	public ActivityControlsDemo()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ControlsDemo/ControlsDemo/TextControlsDemo.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace ControlsDemo;
+
+public partial class TextControlsDemo : ContentPage
+{
+	public TextControlsDemo()
+	{
+		InitializeComponent();
+	}
+
+    void Entry_TextChanged(System.Object sender, Microsoft.Maui.Controls.TextChangedEventArgs e)
+    {
+     
+    }
+
+    void Entry_Completed(System.Object sender, System.EventArgs e)
+    {
+        Debug.WriteLine(txtName.Text);
+    }
+}
