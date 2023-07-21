39b9d56a9ec4d3ef9b65e199a73bbec9dc972612
Ranjith <Ranjith.Rajendiran-ext@bshg.com>
2023-07-21 15:09:44 +0530

    External Resource-how-to-use demo added

---
+++ ExternalResourcesDemo/ExternalResourcesDemo/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace ExternalResourcesDemo;
+using System.Text.Json;
+
+public partial class MainPage : ContentPage
+{
+	
+	public MainPage()
+	{
+		InitializeComponent();
+        
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        LoadMauiAsset();
+    }
+
+    async Task LoadMauiAsset()
+    {
+        using var stream = await FileSystem.OpenAppPackageFileAsync("data.json");
+        using var reader = new StreamReader(stream);
+
+        var contents = reader.ReadToEnd();
+        var p = JsonSerializer.Deserialize<Person>(contents);
+
+
+    }
+
+
+}
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+
---
+++ ExternalResourcesDemo/ExternalResourcesDemo/MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace ExternalResourcesDemo;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("Balisay.ttf", "Balisay");
+                fonts.AddFont("GoldenRecord_PERSONAL_USE_ONLY.otf", "GoldenRecord_PERSONAL_USE_ONLY");
+                fonts.AddFont("GoldenRecordOutl_PERSONAL_USE_ONLY.otf", "GoldenRecordOutl_PERSONAL_USE_ONLY");
+                fonts.AddFont("fontello.ttf", "Icons");
+            });
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
+
